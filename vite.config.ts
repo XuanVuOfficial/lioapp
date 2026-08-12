d1f807650fd26acf18fcb054c3c2e615f3ca90de
Trần Xuân Vũ <xuan.vu.official@gmail.com>
2026-08-12 15:26:20 +0700

    Add fail-safe fallback for Firebase Admin SDK

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
-  const version = "1.0.4";
+  const version = "1.0.5";
 
   return {
     plugins: [
